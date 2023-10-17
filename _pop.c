48eea178ae57155299fe94f78cb5134f39c4360e
Fihliwe <siyabulelmncube@gmail.com>
2023-10-17 06:36:27 -0700

    pop file added

---
+++ _pop.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * _pop - removes the top element of the stack.
+ * @head: start of the stack.
+ * @line_number:  print the error message.
+ * Return: nothing
+ */
+
+void _pop(stack_t **head, unsigned int line_number)
+{
+	stack_t x;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fclose(monty.arg);
+		free(monty.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	x = *head;
+	*head = x->next;
+	free(x);
+}		
